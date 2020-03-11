0da61a1b91ab41fdbfaaf039b58a139231d7a01b
BAUMANN Philippe <Philippe.BAUMANN@cpnv.ch>
2020-03-11 11:47:27 +0100

    rename_views

---
+++ app/homepage/homepage.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp.homepage', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/homepage', {
+    templateUrl: 'homepage/homepage.html',
+    controller: 'homepageCtrl'
+  });
+}])
+
+.controller('homepageCtrl', [function() {
+   
+}]);
+
