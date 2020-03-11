10326d51901aed782f02b46c1b074641f1075d37
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-11 12:00:02 +0100

    Add language to index

--- app/app.js
+++ app/app.js
@@ -2,13 +2,32 @@
 
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.homepage',
-  'myApp.search',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngRoute',
+    'myApp.homepage',
+    'myApp.search',
+    'myApp.version'
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/homepage'});
-}]);
+    $routeProvider.otherwise({redirectTo: '/homepage'});
+}]).controller('appController', function ($scope, $http) {
+    $scope.language = {
+        "id": "fr",
+        "name": "Français"
+    };
+    $scope.loadLanguage = function () {
+        $http.get('language/' + $scope.language.id + '.json')
+            .then(function (response) {
+                $scope.textContent = response.data;
+            });
+    };
+    $scope.displayLanguage = function (language) {
+        return language.id.toUpperCase() + "-" + language.name
+    };
+
+    $http.get('language/languages.json')
+        .then(function (response) {
+            $scope.languages = response.data;
+        });
+    $scope.loadLanguage();
+});
