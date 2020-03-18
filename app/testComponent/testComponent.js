fefc88514d013df5211c784094b9bc0e1c0607ae
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-18 20:49:06 +0100

    Add call to parent bindings

--- app/testComponent/testComponent.js
+++ app/testComponent/testComponent.js
@@ -1,5 +1,12 @@
 'use strict';
 
+function TestComponentController($scope, $element, $attrs) {
+    // let controller = this;
+    $scope.content = "Test component";
+    $scope.language = $scope.$parent.language.name;
+}
+
 angular.module('angulago').component('testComponent', {
     templateUrl: 'testComponent/testComponent.html',
+    controller: TestComponentController,
 });
