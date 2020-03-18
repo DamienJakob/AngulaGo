3990a4be044b9f0a764b4052bbd8540ab7fadf26
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-18 20:15:27 +0100

    Update test component

--- app/testComponent/testComponent.js
+++ app/testComponent/testComponent.js
@@ -1,33 +1,5 @@
 'use strict';
 
-function HeroListController($scope, $element, $attrs) {
-    let ctrl = this;
-
-    // This would be loaded by $http etc.
-    ctrl.list = [
-        {
-            name: 'Superman',
-            location: ''
-        },
-        {
-            name: 'Batman',
-            location: 'Wayne Manor'
-        }
-    ];
-
-    ctrl.updateHero = function(hero, prop, value) {
-        hero[prop] = value;
-    };
-
-    ctrl.deleteHero = function(hero) {
-        var idx = ctrl.list.indexOf(hero);
-        if (idx >= 0) {
-            ctrl.list.splice(idx, 1);
-        }
-    };
-}
-
-angular.module('angulago').component('heroList', {
-    templateUrl: 'heroList.html',
-    controller: HeroListController
+angular.module('angulago').component('testComponent', {
+    templateUrl: 'testComponent/testComponent.html',
 });
