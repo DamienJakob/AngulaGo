af2bd46a90945aa7f75009cd145cf34f4c4a2bf1
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-20 08:52:28 +0100

    Delete unused homepage controller

--- app/homepage/homepage.js
+++ app/homepage/homepage.js
@@ -1,14 +1,9 @@
 'use strict';
 
-function HomepageController() {
-
-}
-
 angular.module('angulago.homepage', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/homepage', {
             templateUrl: 'homepage/homepage.html',
-            controller: HomepageController,
         });
     }]);
 
