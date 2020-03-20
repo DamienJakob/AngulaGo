a24f3ac2d95844729d1d3d1b5dbca760a41a9fea
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-20 09:21:14 +0100

    Remove unused controller from tripSearchForm

--- app/tripSearchForm/tripSearchForm.js
+++ app/tripSearchForm/tripSearchForm.js
@@ -1,12 +1,7 @@
 'use strict';
 
-function TripSearchFormController($scope) {
-
-}
-
 angular.module('angulago').component('tripSearchForm', {
     templateUrl: 'tripSearchForm/tripSearchForm.html',
-    controller: TripSearchFormController,
     bindings: {
         textContent: '<',
     }
