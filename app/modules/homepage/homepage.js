f5f457273358e7a2331c9fdb4f96a290e1297b4d
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-21 18:38:32 +0100

    Add languages files per modules

--- app/modules/homepage/homepage.js
+++ app/modules/homepage/homepage.js
@@ -1,8 +1,27 @@
 'use strict';
 
+function HomepageController($scope, $http) {
+    // functions
+    $scope.loadLanguage = function (language = $scope.language) {
+        $http.get('data/languages/homepage/homepage.' + language.id + '.json')
+            .then(function (response) {
+                $scope.textContent = response.data;
+            });
+    };
+
+    // init
+    $scope.loadLanguage();
+
+    // event listeners
+    $scope.$on('setLanguage', function (event, language) {
+        $scope.loadLanguage(language);
+    });
+}
+
 angular.module('angulago.homepage', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/homepage', {
+            controller: HomepageController,
             templateUrl: 'modules/homepage/homepage.html',
         });
     }]);
