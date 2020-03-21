bf7b49c61b45808592d8b776a11d6307ba9f3f6c
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-21 09:47:19 +0100

    Add controller to selectCurrency component

--- app/components/selectCurrency/selectCurrency.js
+++ app/components/selectCurrency/selectCurrency.js
@@ -1,7 +1,23 @@
 'use strict';
 
+function SelectCurrencyController($scope) {
+
+    this.$onInit = () => {
+        $scope.currency = this.textContent.defaultCurrency;
+    };
+
+    // watcher
+    // when textContent is updated, set the currency to the default currency of textContent
+    $scope.$watch('$ctrl.textContent', function (newTextContent, oldTextContent) {
+        if (newTextContent != oldTextContent) {
+            $scope.currency = newTextContent.defaultCurrency;
+        }
+    });
+}
+
 angular.module('angulago').component('selectCurrency', {
     templateUrl: 'components/selectCurrency/selectCurrency.html',
+    controller: SelectCurrencyController,
     bindings: {
         textContent: '<',
     }
