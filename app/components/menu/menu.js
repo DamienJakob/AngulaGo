5fb506994050887db6aa7640671ddb48ad2538e0
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-21 10:27:03 +0100

    Add selectLanguage component

--- app/components/menu/menu.js
+++ app/components/menu/menu.js
@@ -1,24 +1,7 @@
 'use strict';
 
 function MenuController($scope, $http) {
-    // functions
-    $scope.selectLanguage = function (language = $scope.language) {
-        $scope.$emit('loadLanguage', language);
-    };
-    $scope.displayLanguage = function (language) {
-        return language.id.toUpperCase() + "-" + language.name;
-    };
 
-    // init
-    this.$onInit = () => {
-        // get the default languages from parent
-        // we have to wait until the bindings are done
-        $scope.language = this.defaultLanguage;
-        $http.get('data/languages.json')
-            .then(function (response) {
-                $scope.languages = response.data;
-            });
-    };
 }
 
 angular.module('angulago').component('menu', {
